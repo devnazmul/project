2030950304ad4a642256981c9b7ab853f5be49ad
Md Nazmul Islam <kamruz.zaman8991@gmail.com>
2023-01-31 12:39:26 +0600

    fix bug

--- src/components/HomeMobileAppSec/HomeMobileAppSec.jsx
+++ src/components/HomeMobileAppSec/HomeMobileAppSec.jsx
@@ -1,55 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import lrftBgImage from "../../assets/004.svg";
 import phoneImage from "../../assets/phone.svg";
-import sliderImage1 from "../../assets/slider1.svg";
-import sliderImage2 from "../../assets/slider2.svg";
-import sliderImage3 from "../../assets/slider3.svg";
-import sliderImage4 from "../../assets/slider4.svg";
-import sliderImage5 from "../../assets/slider5.svg";
-import sliderImage6 from "../../assets/slider6.svg";
-import sliderImage7 from "../../assets/slider7.svg";
 
 import appStoreImage from "../../assets/appstore.svg";
 import playStoreImage from "../../assets/playstore.svg";
 import QrCodeImage from "../../assets/QR.svg";
 
-// var ReactDOM = require('react-dom');
-// var Carousel = require('react-responsive-carousel').Carousel;
-
 export default function HomeMobileAppSec() {
-  const [imageIndex, setImageIndex] = useState(0);
-
-  const [sliderImages, setSliderImages] = useState([
-    {
-      imageUrl: sliderImage1,
-    },
-    {
-      imageUrl: sliderImage2,
-    },
-    {
-      imageUrl: sliderImage3,
-    },
-    {
-      imageUrl: sliderImage4,
-    },
-    {
-      imageUrl: sliderImage5,
-    },
-    {
-      imageUrl: sliderImage6,
-    },
-    {
-      imageUrl: sliderImage7,
-    },
-  ]);
-
-  setInterval(() => {
-    if (imageIndex < sliderImages.length - 1) {
-      setImageIndex(imageIndex + 1);
-    } else {
-      setImageIndex(0);
-    }
-  }, [5000]);
+ 
   return (
     <div className="py-[100px] relative flex justify-between px-36">
       {/* IMAGE SECTION  */}
@@ -64,47 +22,6 @@ export default function HomeMobileAppSec() {
       <div className="w-10/12">
         <div className="relative h-[650px] w-[295px] top-[50%] -translate-y-[50%]">
           <div className="absolute top-[50%] -translate-y-[50%] right-1 w-[290px] h-[600px] rounded-[50px] bg-[#C62829]" />
-          {/* <Carousel
-            className="absolute top-[50%] -translate-y-[50%] right-1 w-[290px] h-[600px] rounded-[50px]"
-            showArrows={false}
-          >
-            <img
-              src={sliderImages[0]?.imageUrl}
-              className="object-contain  w-[290px] h-[600px] rounded-[50px]"
-              alt=""
-            /> */}
-          {/* <img
-              src={sliderImages[1]?.imageUrl}
-              className="object-contain  w-[290px] h-[600px] rounded-[50px]"
-              alt=""
-            /> */}
-          {/* <img
-              src={sliderImages[2]?.imageUrl}
-              className="object-contain w-[290px] h-[600px] rounded-[50px]"
-              alt=""
-            />
-            <img
-              src={sliderImages[3]?.imageUrl}
-              className="object-contain w-[290px] h-[600px] rounded-[50px]"
-              alt=""
-            />
-            <img
-              src={sliderImages[4]?.imageUrl}
-              className="object-contain  w-[290px] h-[600px] rounded-[50px]"
-              alt=""
-            />
-            <img
-              src={sliderImages[5]?.imageUrl}
-              className="object-contain w-[290px] h-[600px] rounded-[50px]"
-              alt=""
-            /> */}
-          {/* <img
-              src={sliderImages[6]?.imageUrl}
-              className="object-contain  w-[290px] h-[600px] rounded-[50px]"
-              alt=""
-            /> */}
-          {/* </Carousel> */}
-
           <img
             className="absolute top-[50%] -translate-y-[50%] right-1 h-[650px]"
             src={phoneImage}
