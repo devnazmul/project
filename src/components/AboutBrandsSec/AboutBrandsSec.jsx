a6a11a3306b3edf166012115d24c5d8c57d61479
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-15 13:08:52 +0600

    about brand res

--- src/components/AboutBrandsSec/AboutBrandsSec.jsx
+++ src/components/AboutBrandsSec/AboutBrandsSec.jsx
@@ -12,36 +12,36 @@ import yamahaImage from "../../assets/yamaha.png";
 
 export default function AboutBrandsSec() {
   return (
-    <div className="aboutGlass py-8 px-32 my-10">
-      <div className="grid grid-cols-5 gap-1 px-32">
-        <div className="w-[100px] h-[100px]">
+    <div className="aboutGlass py-8 px-6 lg:px-32 my-10">
+      <div className="grid grid-cols-3 lg:grid-cols-5 gap-1 lg:px-32 pl-6">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={bajajImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={hondaImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={heroImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={yamahaImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={mahindraImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={royalInfieldImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={jawaImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={tvsImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={ktmImage} alt="" />
         </div>
-        <div className="w-[100px] h-[100px]">
+        <div className="w-[70px] lg:w-[100px] h-[70px] lg:h-[100px]">
           <img className="object-contain w-full h-full" src={sujukiImage} alt="" />
         </div>
       </div>
--- src/components/AboutOurTeamSec/AboutOurTeamSec.jsx
+++ src/components/AboutOurTeamSec/AboutOurTeamSec.jsx
@@ -3,14 +3,14 @@ import teamImage from "../../assets/team.png";
 
 export default function AboutOurTeamSec() {
   return (
-    <div className="pr-32 flex my-20 relative items-center text-black">
-      <div className={`aboutOurTeamGlass w-8/12 pl-32 h-[450px] pr-52 py-20 `}>
-        <h3 className="text-4xl font-semibold">Our team</h3>
-        <p className="text-justify pt-7">
+    <div className="lg:pr-32 flex my-6 lg:my-20 relative items-center text-black">
+      <div className={`aboutOurTeamGlass w-full lg:w-8/12 lg:pl-32 px-6 lg:h-[450px] lg:pr-52 py-20 `}>
+        <h3 className="text-xl lg:text-4xl font-semibold">Our team</h3>
+        <p className="text-justify pt-2 lg:pt-7 text-sm lg:text-base">
           Bikefixup is India's leading bike service provider, spread into a
           number of cities and states providing you with 500+ best quality
           services for your bikes like repair, servicing, maintenance,
-          inspection, and a lot more. 
+          inspection, and a lot more.
           <br />
           <br />
           Our Team is Dedicated to Enthrall you and
@@ -18,7 +18,7 @@ export default function AboutOurTeamSec() {
           delightful experience with us.
         </p>
       </div>
-      <div className="absolute right-[132px] h-[350px] overflow-hidden w-[500px] rounded-3xl">
+      <div className="absolute right-[-300px] lg:right-[132px] lg:h-[350px] h-[150px] overflow-hidden w-[500px] rounded-3xl z-[-11]">
         <img className="h-full object-cover" src={teamImage} alt="" />
       </div>
     </div>
