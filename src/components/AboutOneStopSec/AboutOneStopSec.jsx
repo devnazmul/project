2d00abcc5eb94a24f4406de72d6f389446961cbb
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-15 13:14:51 +0600

    about done!

--- src/components/AboutOneStopSec/AboutOneStopSec.jsx
+++ src/components/AboutOneStopSec/AboutOneStopSec.jsx
@@ -3,11 +3,11 @@ import AboutSimpleCard from "./AboutSimpleCard";
 
 export default function AboutOneStopSec() {
   return (
-    <div className="aboutOneStepGlass px-32 py-8 text-black">
-      <h3 className="text-center font-semibold text-3xl mb-10">
+    <div className="aboutOneStepGlass px-6 lg:px-32 py-8 text-black">
+      <h3 className="text-center font-semibold lg:text-3xl text-2xl mb-6 lg:mb-10">
         What makes us a one stop ?
       </h3>
-      <div className="flex justify-between">
+      <div className="flex justify-between lg:flex-row flex-col">
         <AboutSimpleCard
           title={`Service Quality Assurance`}
           paragraph={`We at Bikefixup use only originally manufactured products to provide the best possible bike service experience to our customer.Customer satisfaction is our top-most priority, and we provide that satisfaction in term of quality service. We have 500+ happy customer.`}
--- src/components/AboutOneStopSec/AboutSimpleCard.jsx
+++ src/components/AboutOneStopSec/AboutSimpleCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function AboutSimpleCard({ title, paragraph }) {
   return (
-    <div className="aboutGlass pt-8 px-10 w-[470px]">
+    <div className="aboutGlass pt-8 px-10 w-full lg:w-[470px] mb-5">
       <h5 className="text-xl font-semibold text-center mb-5">{title}</h5>
       <p className="font-normal">
         {paragraph.split(".").map((p) => (
