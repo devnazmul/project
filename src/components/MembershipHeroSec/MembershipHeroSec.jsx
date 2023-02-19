459648086d3a60b211c3e7fee7529f11ba4977f2
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-19 11:20:11 +0600

    member hero section responsive

--- src/components/MembershipHeroSec/MembershipHeroSec.jsx
+++ src/components/MembershipHeroSec/MembershipHeroSec.jsx
@@ -4,20 +4,20 @@ import membershipHeroImage from '../../assets/membershipHero.svg';
 
 export default function MembershipHeroSec() {
   return (
-    <div className="px-32 py-7 text-black">
-      <h1 className="text-[50px] text-center font-semibold">
+    <div className="lg:px-32 px-2 lg:py-7 py-2 text-black">
+      <h1 className="lg:text-[50px] text-xl text-center font-semibold">
         <span>
           Bike<span className="text-primaryRed">Flxup</span>
         </span>{" "}
         Online Membership
       </h1>
-      <div className="flex my-10 justify-between">
+      <div className="flex flex-col lg:flex-row my-10 justify-between ">
         <img className="w-[450px]" src={membershipHeroImage} alt={"Online Membership"} />
         <div className="heroGlass py-5 px-10">
-          <h3 className="text-3xl mb-5 text-primaryPurple font-semibold">
+          <h3 className="lg:text-3xl text-lg lg:mb-5 mb-3 text-primaryPurple font-semibold">
             Save Thousands Annually on <br /> Your Vehicle Servicing
           </h3>
-          <p>
+          <p className="text-xs lg:text-base">
             BikeFixup provides top-notch service and benefits to make your
             vehicle maintenance more affordable, convenient & stress-free. With
             our wide service network, we offer premium and smart maintenance,
