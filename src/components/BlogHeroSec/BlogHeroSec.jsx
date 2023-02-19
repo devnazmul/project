8d7f258bc29efe514788e95e4fc756a80f1b0fc3
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-19 11:57:19 +0600

    blog post responsive done

--- src/components/BlogHeroSec/BlogHeroSec.jsx
+++ src/components/BlogHeroSec/BlogHeroSec.jsx
@@ -3,10 +3,10 @@ import BlogHeroImage from "../../assets/blogHero.svg";
 
 export default function BlogHeroSec() {
   return (
-    <div className="px-36 flex ">
+    <div className="lg:px-36 px-6 flex lg:flex-row flex-col-reverse">
       <div>
-        <h1 className="text-[50px] font-semibold text-primaryPurple">Your Blog Title Here.</h1>
-        <p className="pr-36">
+        <h1 className="lg:text-[50px] text-3xl font-semibold text-primaryPurple">Your Blog Title Here.</h1>
+        <p className="lg:pr-36 pr-0 mt-3">
           consectetur adipiscing elit, sed do eiusmod tempor incididuntLorem
           ipsum dolor sit amet
         </p>
