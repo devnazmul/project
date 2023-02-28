f18b3f14e7ff438b11ed676c4f258e6b0ec1fe4e
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2023-02-28 10:09:16 +0600

    Update firebase.config.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,11 +2,7 @@
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey:process.env.REACT_APP_apiKey,
   authDomain:process.env.REACT_APP_authDomain,
